822dafb126664254c637eb22ffd545543f6b1ce3
chrialge <chrialge@gmail.com>
2024-03-19 16:06:38 +0100

    add list of mail and visible when i have 10 mail

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
-console.log('ciao')
+console.log("ciao");
+
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            emails: [],
+            ciao: 'ciao',
+            counter: 0
+        };
+    },
+    methods:{
+        createEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                    console.log(response.data.response);
+                    email = response.data.response;
+                    this.emails.push(email)
+                    return email
+            });
+        }
+    },
+    mounted(){
+        
+        while (this.counter < 9) {
+            this.createEmail()
+            this.counter++
+        }
+        console.log(this.counter)
+        console.log(this.createEmail())
+        console.log(this.emails)
+    }
+}).mount("#app");
